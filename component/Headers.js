82402c325ab1335736d6cba79295ca6e64da866e
Malik04121 <shehzadmalik123.sm@gmail.com>
2022-09-30 16:48:56 +0530

    navbar complete

---
+++ component/Headers.js
@@ -0,0 +1,15 @@
+let headers=()=>{
+    return` <a href="./home.html"><img id="homeimg" src="https://assets.milaap.org/assets/milaap-logo-tiny-4d3dbc4e55c2aaec351f0f376329c624236c4d6266b910f472684e70603f600d.png"></a>
+<div id="left">
+    
+    <h3><a href="">Home</a></h3>
+    <h3><a href="">Donate</a></h3>
+    <h3><a href="">Lend</a></h3>
+    <h3><a href="">Pricing</a></h3>
+    <h3><a href="">Contact us</a></h3>
+    
+</div>
+    <button >Start a fundraiser</button>
+    <img id="reg" src="https://cdn.icon-icons.com/icons2/2506/PNG/512/user_icon_150670.png" alt="">`
+}
+export default headers
