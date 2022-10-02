a3ae1f6539780f6a20f187ab67eae8ba4ff2f3d9
rakeshrauniyar12 <rakeshrauniyar12>
2022-10-02 09:13:54 +0530

    added

--- signup/signup.js
+++ signup/signup.js
@@ -7,9 +7,27 @@ let signup=()=>{
     let password=document.getElementById("pass").value
     
     let data_obj={name,email,password}
-    data_arr.push(data_obj)
+    if(checkUser(data_obj)===true){
+        data_arr.push(data_obj)
     localStorage.setItem("signup",JSON.stringify(data_arr))
+    alert("You are Successfully Signup");
+    } else {
+        alert("Already Signup");
+    }
+    document.getElementById("name").value="";
+    document.getElementById("email").value="";
+    document.getElementById("pass").value="";
 
 
-
+}
+let checkUser=(e)=>{
+     let filtered=data_arr.filter((el)=>{
+        return e.email===el.email;
+     })
+     if(filtered.length>0){
+        return false;
+     }
+     else {
+        return true;
+     }
 }
